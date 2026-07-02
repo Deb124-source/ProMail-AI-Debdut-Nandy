fb050a0ea0086f7262b45e75c51f0d06e9797fd4
Debdut Nandy <debdut937@gmail.com>
2026-07-02 16:22:05 +0530

    Create About.py

---
+++ pages/About.py
@@ -0,0 +1,38 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="About",
+    page_icon="",
+    layout="wide"
+)
+
+st.title("About MailGenius AI")
+
+st.markdown("""
+## ProMail AI
+
+An AI-powered professional email assistant built using:
+
+- Google Gemini 2.5 Flash
+- Streamlit
+- Python
+
+###  Features
+
+-  Generate Emails
+-  Reply Emails
+-  Improve Emails
+-  Grammar Checker
+-  Email Summarizer
+-  Translator
+-  Tone Changer
+-  Email Analyzer
+-  Signature Generator
+-  Smart Templates
+-  Subject Generator
+-  Email Quality Scorer
+
+---
+
+Developed by **Debdut Nandy**
+""")
